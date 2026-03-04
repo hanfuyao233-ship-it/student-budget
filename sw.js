d180fbd81adf85e4ac6739d2ff8328ef56b9493d
hanfuyao233-ship-it <hanfuyao233@gmail.com>
2026-03-04 13:02:37 +0800

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// 安装时缓存文件
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// 拦截请求，优先返回缓存
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
